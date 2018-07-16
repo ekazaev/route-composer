b1210363af20043464a90bf0bc765efaf66d4b5c
Eugene Kazaev <ekazaev@gilt.com>
2018-07-16 12:59:02 +0100

    Added an ability to create inline interceptor, context and post-routing tasks.

--- Example/RouteComposer/AppDelegate.swift
+++ Example/RouteComposer/AppDelegate.swift
@@ -11,21 +11,21 @@ import RouteComposer
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     
     let transitionDelegate = BlurredBackgroundPresentationController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
+        
         configureNavigationUsingDictionaryConfig()
-
+        
         return true
     }
-
+    
     private func configureNavigationUsingDictionaryConfig() {
         //As one of examples configuration can be stored in one configuration object. Other configs are in CitiesConfiguration, Product configuration and LoginConfiguration as static objects
-
+        
         // Home Tab Bar Screen
         let homeScreen = StepAssembly(
                 // Because both factory and finder are Generic, You have to provide to at least one instance
@@ -37,9 +37,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(ExampleAnalyticsPostAction())
                 .from(RootViewControllerStep())
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: homeScreen, for: ExampleTarget.home)
-
+        
         // Square Tab Bar Screen
         let squareScreen = StepAssembly(
                 finder: ClassFinder<SquareViewController, ExampleDictionaryContext>(options: .currentAllStack),
@@ -49,9 +49,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(ExampleAnalyticsPostAction())
                 .from(homeScreen)
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: squareScreen, for: ExampleTarget.square)
-
+        
         // Circle Tab Bar screen
         let circleScreen = StepAssembly(
                 finder: ClassFinder<CircleViewController, ExampleDictionaryContext>(options: .currentAllStack),
@@ -61,22 +61,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(ExampleAnalyticsPostAction())
                 .from(homeScreen)
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: circleScreen, for: ExampleTarget.circle)
-
+        
         //Color screen
         let colorScreen = StepAssembly(
                 finder: ColorViewControllerFinder(),
                 factory: ColorViewControllerFactory(action: NavigationControllerFactory.PushToNavigation()))
                 .add(ExampleAnalyticsInterceptor())
+                .add(InlineInterceptor({ (d: ExampleDestination) in
+                    print("On before navigation to Color view controller")
+                }))
                 .add(ExampleGenericContextTask())
                 .add(ExampleAnalyticsPostAction())
+                .add(InlineContextTask({ (vc: ColorViewController, context: ExampleDictionaryContext) in
+                    print("Color view controller built or found")
+                }))
+                .add(InlinePostTask({ (vc:ColorViewController, d: ExampleDestination, _) in
+                    print("After navigation to Color view controller")
+                }))
                 .from(NavigationControllerStep(action: GeneralAction.PresentModally(transitioningDelegate: transitionDelegate)))
                 .from(CurrentViewControllerStep())
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: colorScreen, for: ExampleTarget.color)
-
+        
         //Star screen
         let starScreen = StepAssembly(
                 finder: ClassFinder<StarViewController, Any>(options: .currentAllStack),
@@ -87,9 +96,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(LoginInterceptor())
                 .from(homeScreen)
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: starScreen, for: ExampleTarget.star)
-
+        
         //Screen with Routing support
         let routingSupportScreen = StepAssembly(
                 finder: ClassFinder<RoutingRuleSupportViewController, Any?>(options: .currentAllStack),
@@ -99,10 +108,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(ExampleAnalyticsPostAction())
                 .from(colorScreen)
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: routingSupportScreen,
                 for: ExampleTarget.ruleSupport)
-
+        
         // Empty Screen
         let emptyScreen = StepAssembly(
                 finder: ClassFinder<EmptyViewController, Any?>(),
@@ -113,9 +122,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(ExampleAnalyticsPostAction())
                 .from(circleScreen)
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: emptyScreen, for: ExampleTarget.empty)
-
+        
         // Two modal presentations in a row screen
         let superModalScreen = StepAssembly(
                 finder: ClassFinder<SecondModalLevelViewController, Any?>(),
@@ -126,9 +135,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .from(NavigationControllerStep(action: GeneralAction.PresentModally()))
                 .from(routingSupportScreen)
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: superModalScreen, for: ExampleTarget.secondLevelModal)
-
+        
         // Welcome Screen
         let welcomeScreen = StepAssembly(
                 finder: ClassFinder<PromptViewController, Any?>(),
@@ -138,14 +147,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 .add(ExampleAnalyticsPostAction())
                 .from(RootViewControllerStep())
                 .assemble()
-
+        
         ExampleConfiguration.register(screen: welcomeScreen, for: ExampleTarget.welcome)
-
+        
         ExampleUniversalLinksManager.register(translator: ColorURLTranslator())
         ExampleUniversalLinksManager.register(translator: ProductURLTranslator())
         ExampleUniversalLinksManager.register(translator: CityURLTranslator())
     }
-
+    
     func application(_ application: UIApplication,
                      open url: URL,
                      sourceApplication: String?,
@@ -153,7 +162,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         guard let destination = ExampleUniversalLinksManager.destination(for: url) else {
             return false
         }
-
+        
         return DefaultRouter(logger: nil).deepLinkTo(destination: destination) == .handled
     }
 }
---
+++ RouteComposer/Classes/InlineTasks.swift
@@ -0,0 +1,107 @@
+//
+//  InlineTasks.swift
+//  RouteComposer
+//
+//  Created by Eugene Kazaev on 16/07/2018.
+//
+
+import Foundation
+import UIKit
+
+/// `InlineInterceptor` is the inline interceptor.
+///
+/// **NB:** We would recommend it for the purpose of configuration testing, but then replace it with a strongly typed
+/// `RoutingInterceptor` instance.
+public struct InlineInterceptor<D: RoutingDestination>: RoutingInterceptor {
+    
+    public typealias Destination = D
+    
+    private let asyncCompletion: ((_: D, _: @escaping (InterceptorResult) -> Void) -> Void)?
+    
+    private let syncCompletion: ((_: D) -> Void)?
+    
+    /// Constructor
+    ///
+    /// - Parameter completion: the block to be called when `InlineInterceptor` will take a control over the routing.
+    ///
+    ///     **NB** For `Router` to be able to continue routing, completion block method **MUST** to be called.
+    public init(_ completion: @escaping (_: D, _: @escaping (InterceptorResult) -> Void) -> Void) {
+        self.asyncCompletion = completion
+        self.syncCompletion = nil
+    }
+
+    /// Constructor
+    ///
+    /// - Parameter completion: the block to be called when `InlineInterceptor` will take a control over the routing.
+    ///
+    ///     **NB** completion method will be called automatically so do not use this constructor if your interceptor
+    ///     task is asynchronous.
+    public init(_ completion: @escaping (_: D) -> Void) {
+        self.syncCompletion = completion
+        self.asyncCompletion = nil
+    }
+    
+    public func execute(for destination: D, completion: @escaping (InterceptorResult) -> Void) {
+        if let syncCompletion = syncCompletion {
+            syncCompletion(destination)
+            completion(.success)
+        } else if let asyncCompletion = asyncCompletion {
+            asyncCompletion(destination, completion)
+        } else {
+            completion(.failure("No completion block provided."))
+        }
+    }
+    
+}
+
+/// `InlineContextTask` is the inline context task.
+///
+/// **NB:** We would recommend it for the purpose of configuration testing, but then replace it with a strongly typed
+/// `ContextTask` instance.
+public struct InlineContextTask<VC: UIViewController, C>: ContextTask {
+    
+    public typealias ViewController = VC
+    
+    public typealias Context = C
+    
+    private let completion: (_: VC, _: C) -> Void
+    
+    /// Constructor
+    ///
+    /// - Parameter completion: the block to be called when `InlineContextTask` will be applied to the `UIViewController`
+    ///    instance.
+    public init(_ completion: @escaping (_: VC, _: C) -> Void) {
+        self.completion = completion
+    }
+    
+    public func apply(on viewController: ViewController, with context: Context) {
+        completion(viewController, context)
+    }
+    
+}
+
+/// `InlinePostTask` is the inline context task.
+///
+/// **NB:** We would recommend it for the purpose of configuration testing, but then replace it with a strongly typed
+/// `PostRoutingTask` instance.
+public struct InlinePostTask<VC: UIViewController, D: RoutingDestination>: PostRoutingTask {
+    
+    public typealias ViewController = VC
+    
+    public typealias Destination = D
+    
+    private let completion: (_: VC, _: D, _: [UIViewController]) -> Void
+    
+    /// Constructor
+    ///
+    /// - Parameter completion: the block to be called when `InlinePostTask` will be called at the end of the routing
+    ///    process.
+    public init(_ completion: @escaping (_: VC, _: D, _: [UIViewController]) -> Void) {
+        self.completion = completion
+    }
+    
+    public func execute(on viewController: ViewController, for destination: Destination, routingStack: [UIViewController]) {
+        completion(viewController, destination, routingStack)
+    }
+    
+}
