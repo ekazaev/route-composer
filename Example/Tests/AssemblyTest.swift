88bd08d41f853964735826f1e8d378ae239ec609
Eugene Kazaev <ekazaev@gilt.com>
2018-09-20 00:02:33 +0100

    Updated unit test

--- Example/Tests/AssemblyTest.swift
+++ Example/Tests/AssemblyTest.swift
@@ -1,116 +1,116 @@
-////
-//// Created by Eugene Kazaev on 12/09/2018.
-//// Copyright (c) 2018 Gilt Groupe. All rights reserved.
-////
 //
-//import UIKit
-//import Foundation
-//import XCTest
-//@testable import RouteComposer
+// Created by Eugene Kazaev on 12/09/2018.
+// Copyright (c) 2018 Gilt Groupe. All rights reserved.
 //
-//class AssemblyTest: XCTestCase {
-//
-//    func testStepAssembly() {
-//        let lastStepAssembly = StepAssembly(finder: ClassFinder<UIViewController, Any?>(), factory: XibFactory(nibName: "AnyNibName"))
-//                .using(GeneralAction.nilAction())
-//                .from(NavigationControllerStep())
-//                .using(GeneralAction.presentModally())
-//        XCTAssertEqual(lastStepAssembly.previousSteps.count, 2)
-//
-//        var currentStep: RoutingStep? = lastStepAssembly.assemble(from: GeneralStep.current())
-//        var chainedStepCount = 0
-//        while currentStep != nil {
-//            chainedStepCount += 1
-//            if let chainableStep = currentStep as? ChainableStep {
-//                currentStep = chainableStep.previousStep
-//            } else {
-//                currentStep = nil
-//            }
-//        }
-//        XCTAssertEqual(chainedStepCount, 5)
-//    }
-//
-//    func testContainerStepAssembly() {
-//        let lastStepAssembly = ContainerStepAssembly(finder: ClassFinder(), factory: NavigationControllerFactory())
-//                .using(GeneralAction.nilAction())
-//                .from(TabBarControllerStep())
-//                .using(GeneralAction.presentModally())
-//                .from(GeneralStep.root())
-//        XCTAssertEqual(lastStepAssembly.previousSteps.count, 3)
-//
-//        var currentStep: RoutingStep? = lastStepAssembly.assemble()
-//        var chainedStepCount = 0
-//        while currentStep != nil {
-//            chainedStepCount += 1
-//            if let chainableStep = currentStep as? ChainableStep {
-//                currentStep = chainableStep.previousStep
-//            } else {
-//                currentStep = nil
-//            }
-//        }
-//        XCTAssertEqual(chainedStepCount, 5)
-//    }
-//
-//    func testChainAssembly() {
-//        let destinationStep = ChainAssembly.from(NavigationControllerStep())
-//                .using(GeneralAction.presentModally())
-//                .from(GeneralStep.root())
-//                .assemble()
-//        var currentStep: RoutingStep? = ChainAssembly.from(
-//                        destinationStep)
-//                .assemble()
-//
-//        var chainedStepCount = 0
-//        while currentStep != nil {
-//            chainedStepCount += 1
-//            if let chainableStep = currentStep as? ChainableStep {
-//                currentStep = chainableStep.previousStep
-//            } else {
-//                currentStep = nil
-//            }
-//        }
-//        XCTAssertEqual(chainedStepCount, 5)
-//    }
-//
-//    func testCompleteFactoryAssembly() {
-//        let container = CompleteFactoryAssembly(factory: TabBarControllerFactory())
-//                .with(ClassNameFactory<UIViewController, Any?>())
-//                .with(ClassNameFactory<UIViewController, Any?>(), using: TabBarControllerFactory.addTab())
-//                .with(CompleteFactoryAssembly(factory: TabBarControllerFactory())
-//                        .with(ClassNameFactory<UIViewController, Any?>()
-//                        ).assemble(),
-//                        using: TabBarControllerFactory.addTab(at: 1, replacing: true))
-//                .assemble()
-//        XCTAssertEqual(container.childFactories.count, 3)
-//        let tabBarController = try? container.build()
-//        XCTAssertEqual(tabBarController?.viewControllers?.count, 2)
-//    }
-//
-//    func testSwitchAssembly() {
-//        let step = SwitchAssembly<Any?>()
-//                .addCase(when: ClassFinder<UINavigationController, Any?>())
-//                .addCase(when: ClassFinder<UITabBarController, Any?>(), do: GeneralStep.root())
-//                .addCase({ (_: Any?) in
-//                    return GeneralStep.current()
-//                })
-//                .assemble(default: {
-//                    return GeneralStep.root()
-//                }).previousStep as? SwitcherStep<Any?>
-//
-//        XCTAssertNotNil(step)
-//        XCTAssertEqual(step?.resolvers.count, 4)
-//    }
-//
-//    func testStepWithActionAssembly() {
-//        let assembly = StepWithActionAssembly<ClassFinder<RouterTests.TestViewController, Any?>, ClassNameFactory<RouterTests.TestViewController, Any?>>()
-//                .add(InlineInterceptor({ (_: Any?) in
-//                }))
-//                .add(InlineContextTask({ (_: RouterTests.TestViewController, _: Any?) in
-//                }))
-//                .add(InlinePostTask({ (_: RouterTests.TestViewController, _: Any?, _: [UIViewController]) in
-//                }))
-//        XCTAssertNotNil(assembly.taskCollector.interceptor())
-//        XCTAssertNotNil(assembly.taskCollector.contextTask())
-//        XCTAssertNotNil(assembly.taskCollector.postTask())
-//    }
-//}
+
+import UIKit
+import Foundation
+import XCTest
+@testable import RouteComposer
+
+class AssemblyTest: XCTestCase {
+
+    func testStepAssembly() {
+        let lastStepAssembly = StepAssembly(finder: ClassFinder<UIViewController, Any?>(), factory: XibFactory(nibName: "AnyNibName"))
+                .using(NavigationControllerFactory.pushToNavigation())
+                .from(NavigationControllerStep())
+                .using(GeneralAction.presentModally())
+        XCTAssertEqual(lastStepAssembly.previousSteps.count, 2)
+
+        var currentStep: RoutingStep? = lastStepAssembly.assemble(from: GeneralStep.current())
+        var chainedStepCount = 0
+        while currentStep != nil {
+            chainedStepCount += 1
+            if let chainableStep = currentStep as? ChainableStep {
+                currentStep = chainableStep.previousStep
+            } else {
+                currentStep = nil
+            }
+        }
+        XCTAssertEqual(chainedStepCount, 5)
+    }
+
+    func testContainerStepAssembly() {
+        let lastStepAssembly = ContainerStepAssembly(finder: ClassFinder(), factory: NavigationControllerFactory())
+                .using(TabBarControllerFactory.addTab())
+                .from(TabBarControllerStep())
+                .using(GeneralAction.presentModally())
+                .from(GeneralStep.root())
+        XCTAssertEqual(lastStepAssembly.previousSteps.count, 3)
+
+        var currentStep: RoutingStep? = lastStepAssembly.assemble()
+        var chainedStepCount = 0
+        while currentStep != nil {
+            chainedStepCount += 1
+            if let chainableStep = currentStep as? ChainableStep {
+                currentStep = chainableStep.previousStep
+            } else {
+                currentStep = nil
+            }
+        }
+        XCTAssertEqual(chainedStepCount, 5)
+    }
+
+    func testChainAssembly() {
+        let destinationStep = ChainAssembly.from(NavigationControllerStep())
+                .using(GeneralAction.presentModally())
+                .from(GeneralStep.root())
+                .assemble()
+        var currentStep: RoutingStep? = ChainAssembly.from(
+                        destinationStep)
+                .assemble()
+
+        var chainedStepCount = 0
+        while currentStep != nil {
+            chainedStepCount += 1
+            if let chainableStep = currentStep as? ChainableStep {
+                currentStep = chainableStep.previousStep
+            } else {
+                currentStep = nil
+            }
+        }
+        XCTAssertEqual(chainedStepCount, 5)
+    }
+
+    func testCompleteFactoryAssembly() {
+        let container = CompleteFactoryAssembly(factory: TabBarControllerFactory())
+                .with(ClassNameFactory<UIViewController, Any?>())
+                .with(ClassNameFactory<UIViewController, Any?>(), using: TabBarControllerFactory.addTab())
+                .with(CompleteFactoryAssembly(factory: TabBarControllerFactory())
+                        .with(ClassNameFactory<UIViewController, Any?>()
+                        ).assemble(),
+                        using: TabBarControllerFactory.addTab(at: 1, replacing: true))
+                .assemble()
+        XCTAssertEqual(container.childFactories.count, 3)
+        let tabBarController = try? container.build()
+        XCTAssertEqual(tabBarController?.viewControllers?.count, 2)
+    }
+
+    func testSwitchAssembly() {
+        let step = SwitchAssembly<UIViewController, Any?>()
+                .addCase(when: ClassFinder<UINavigationController, Any?>())
+                .addCase(when: ClassFinder<UITabBarController, Any?>(), do: GeneralStep.root())
+                .addCase({ (_: Any?) in
+                    return GeneralStep.current()
+                })
+                .assemble(default: {
+                    return GeneralStep.root()
+                }).previousStep as? SwitcherStep<Any?>
+
+        XCTAssertNotNil(step)
+        XCTAssertEqual(step?.resolvers.count, 4)
+    }
+
+    func testStepWithActionAssembly() {
+        let assembly = StepWithActionAssembly<ClassFinder<RouterTests.TestViewController, Any?>, ClassNameFactory<RouterTests.TestViewController, Any?>>()
+                .add(InlineInterceptor({ (_: Any?) in
+                }))
+                .add(InlineContextTask({ (_: RouterTests.TestViewController, _: Any?) in
+                }))
+                .add(InlinePostTask({ (_: RouterTests.TestViewController, _: Any?, _: [UIViewController]) in
+                }))
+        XCTAssertNotNil(assembly.taskCollector.interceptor())
+        XCTAssertNotNil(assembly.taskCollector.contextTask())
+        XCTAssertNotNil(assembly.taskCollector.postTask())
+    }
+}
--- Example/Tests/BoxTest.swift
+++ Example/Tests/BoxTest.swift
@@ -1,112 +1,112 @@
-////
-//// Created by Eugene Kazaev on 25/07/2018.
-//// Copyright (c) 2018 Gilt Groupe. All rights reserved.
-////
 //
-//import UIKit
-//import XCTest
-//@testable import RouteComposer
+// Created by Eugene Kazaev on 25/07/2018.
+// Copyright (c) 2018 Gilt Groupe. All rights reserved.
 //
-//class BoxTests: XCTestCase {
-//
-//    func testFactoryBox() {
-//        let factory = EmptyFactory()
-//        XCTAssertNotNil(FactoryBox.box(for: factory, action: ActionBox(GeneralAction.nilAction())))
-//    }
-//
-//    func testContainerBox() {
-//        let factory = EmptyContainer()
-//        XCTAssertNotNil(ContainerFactoryBox.box(for: factory, action: ActionBox(GeneralAction.nilAction())))
-//    }
-//
-//    func testNilFactoryBox() {
-//        let factory = NilFactory<UIViewController, Any?>()
-//        XCTAssertNil(FactoryBox.box(for: factory, action: ActionBox(GeneralAction.nilAction())))
-//    }
-//
-//    func testContainerBoxChildrenScrape() {
-//        let factory = EmptyContainer()
-//        var box = ContainerFactoryBox.box(for: factory, action: ActionBox(GeneralAction.nilAction())) as? ContainerFactoryBox<EmptyContainer>
-//        XCTAssertNotNil(box)
-//        var children: [AnyFactory] = []
-//        children.append(FactoryBox.box(for: EmptyFactory(), action: ContainerActionBox(NavigationControllerFactory.pushToNavigation()))!)
-//        children.append(FactoryBox.box(for: EmptyFactory(), action: ContainerActionBox(NavigationControllerFactory.pushToNavigation()))!)
-//        children.append(FactoryBox.box(for: EmptyFactory(), action: ActionBox(GeneralAction.nilAction()))!)
-//
-//        let resultChildren = try? box?.scrapeChildren(from: children)
-//        XCTAssertEqual(resultChildren??.count, 1)
-//        XCTAssertEqual(box?.children.count, 2)
-//    }
-//
-//    func testNilInAssembly() {
-//        let routingStep = StepAssembly(finder: NilFinder<UIViewController, Any?>(),
-//                factory: NilFactory<UIViewController, Any?>())
-//                .using(GeneralAction.nilAction())
-//                .from(GeneralStep.current())
-//                .assemble()
-//        let step = routingStep.previousStep as? BaseStep<FactoryBox<NilFactory<UIViewController, Any?>>>
-//        XCTAssertNotNil(step)
-//        XCTAssertNil(step?.factory)
-//        XCTAssertNil(step?.finder)
-//    }
-//
-//    func testNilInCompleteFactoryAssembly() {
-//        let factory = CompleteFactoryAssembly(factory: TabBarControllerFactory())
-//                .with(NilFactory<UIViewController, Any?>(), using: TabBarControllerFactory.addTab())
-//                .with(NilFactory<UIViewController, Any?>(), using: TabBarControllerFactory.addTab())
-//                .assemble()
-//        XCTAssertEqual(factory.childFactories.count, 0)
-//    }
-//
-//    func testActionBox() {
-//
-//        class TestAction: Action {
-//
-//            func perform(with viewController: UIViewController, on existingController: UINavigationController, animated: Bool, completion: @escaping (ActionResult) -> Void) {
-//                existingController.viewControllers = [viewController]
-//            }
-//
-//        }
-//        let action = TestAction()
-//        let actionBox = ActionBox(action)
-//        let navigationController = UINavigationController()
-//        actionBox.perform(with: UIViewController(), on: navigationController, animated: true) { result in
-//            guard case .continueRouting = result else {
-//                XCTAssert(false)
-//                return
-//            }
-//        }
-//        XCTAssertEqual(navigationController.viewControllers.count, 1)
-//    }
-//
-//    func testContainerActionBox() {
-//
-//        class TestContainerAction: ContainerAction {
-//
-//            typealias SupportedContainer = NavigationControllerFactory
-//
-//            func perform(embedding viewController: UIViewController, in childViewControllers: inout [UIViewController]) {
-//                childViewControllers.append(viewController)
-//            }
-//
-//            func perform(with viewController: UIViewController, on existingController: UINavigationController, animated: Bool, completion: @escaping (ActionResult) -> Void) {
-//                existingController.viewControllers = [viewController]
-//            }
-//
-//        }
-//        let action = TestContainerAction()
-//        let actionBox = ActionBox(action)
-//        let navigationController = UINavigationController()
-//        actionBox.perform(with: UIViewController(), on: navigationController, animated: true) { result in
-//            guard case .continueRouting = result else {
-//                XCTAssert(false)
-//                return
-//            }
-//        }
-//        XCTAssertEqual(navigationController.childViewControllers.count, 1)
-//
-//        actionBox.perform(embedding: UIViewController(), in: &navigationController.viewControllers)
-//        XCTAssertEqual(navigationController.childViewControllers.count, 2)
-//    }
-//
-//}
+
+import UIKit
+import XCTest
+@testable import RouteComposer
+
+class BoxTests: XCTestCase {
+
+    func testFactoryBox() {
+        let factory = EmptyFactory()
+        XCTAssertNotNil(FactoryBox.box(for: factory, action: ActionBox(UIViewController.NilAction())))
+    }
+
+    func testContainerBox() {
+        let factory = EmptyContainer()
+        XCTAssertNotNil(ContainerFactoryBox.box(for: factory, action: ActionBox(UIViewController.NilAction())))
+    }
+
+    func testNilFactoryBox() {
+        let factory = NilFactory<UIViewController, Any?>()
+        XCTAssertNil(FactoryBox.box(for: factory, action: ActionBox(UIViewController.NilAction())))
+    }
+
+    func testContainerBoxChildrenScrape() {
+        let factory = EmptyContainer()
+        var box = ContainerFactoryBox.box(for: factory, action: ActionBox(UIViewController.NilAction())) as? ContainerFactoryBox<EmptyContainer>
+        XCTAssertNotNil(box)
+        var children: [AnyFactory] = []
+        children.append(FactoryBox.box(for: EmptyFactory(), action: ContainerActionBox(NavigationControllerFactory.pushToNavigation()))!)
+        children.append(FactoryBox.box(for: EmptyFactory(), action: ContainerActionBox(NavigationControllerFactory.pushToNavigation()))!)
+        children.append(FactoryBox.box(for: EmptyFactory(), action: ActionBox(UIViewController.NilAction()))!)
+
+        let resultChildren = try? box?.scrapeChildren(from: children)
+        XCTAssertEqual(resultChildren??.count, 1)
+        XCTAssertEqual(box?.children.count, 2)
+    }
+
+    func testNilInAssembly() {
+        let routingStep = StepAssembly(finder: NilFinder<UIViewController, Any?>(),
+                factory: NilFactory<UIViewController, Any?>())
+                .using(UIViewController.NilAction())
+                .from(GeneralStep.current())
+                .assemble()
+        let step = routingStep.previousStep as? BaseStep<FactoryBox<NilFactory<UIViewController, Any?>>>
+        XCTAssertNotNil(step)
+        XCTAssertNil(step?.factory)
+        XCTAssertNil(step?.finder)
+    }
+
+    func testNilInCompleteFactoryAssembly() {
+        let factory = CompleteFactoryAssembly(factory: TabBarControllerFactory())
+                .with(NilFactory<UIViewController, Any?>(), using: TabBarControllerFactory.addTab())
+                .with(NilFactory<UIViewController, Any?>(), using: TabBarControllerFactory.addTab())
+                .assemble()
+        XCTAssertEqual(factory.childFactories.count, 0)
+    }
+
+    func testActionBox() {
+
+        class TestAction: Action {
+
+            func perform(with viewController: UIViewController, on existingController: UINavigationController, animated: Bool, completion: @escaping (ActionResult) -> Void) {
+                existingController.viewControllers = [viewController]
+            }
+
+        }
+        let action = TestAction()
+        let actionBox = ActionBox(action)
+        let navigationController = UINavigationController()
+        actionBox.perform(with: UIViewController(), on: navigationController, animated: true) { result in
+            guard case .continueRouting = result else {
+                XCTAssert(false)
+                return
+            }
+        }
+        XCTAssertEqual(navigationController.viewControllers.count, 1)
+    }
+
+    func testContainerActionBox() {
+
+        class TestContainerAction: ContainerAction {
+
+            typealias SupportedContainer = NavigationControllerFactory
+
+            func perform(embedding viewController: UIViewController, in childViewControllers: inout [UIViewController]) {
+                childViewControllers.append(viewController)
+            }
+
+            func perform(with viewController: UIViewController, on existingController: UINavigationController, animated: Bool, completion: @escaping (ActionResult) -> Void) {
+                existingController.viewControllers = [viewController]
+            }
+
+        }
+        let action = TestContainerAction()
+        let actionBox = ActionBox(action)
+        let navigationController = UINavigationController()
+        actionBox.perform(with: UIViewController(), on: navigationController, animated: true) { result in
+            guard case .continueRouting = result else {
+                XCTAssert(false)
+                return
+            }
+        }
+        XCTAssertEqual(navigationController.childViewControllers.count, 1)
+
+        actionBox.perform(embedding: UIViewController(), in: &navigationController.viewControllers)
+        XCTAssertEqual(navigationController.childViewControllers.count, 2)
+    }
+
+}
