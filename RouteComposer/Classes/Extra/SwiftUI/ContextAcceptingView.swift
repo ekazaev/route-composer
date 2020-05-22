7ebf5ec685c688f7adf207a2bfac83ca170ed371
Eugene Kazaev <eugene.kazaev@gmail.com>
2020-05-22 12:44:37 +0100

    Updated documentation

--- RouteComposer/Classes/Extra/SwiftUI/ContextAcceptingView.swift
+++ RouteComposer/Classes/Extra/SwiftUI/ContextAcceptingView.swift
@@ -1,5 +1,10 @@
 //
-// Created by Eugene Kazaev on 22/05/2020.
+// RouteComposer
+// ContextAcceptingView.swift
+// https://github.com/ekazaev/route-composer
+//
+// Created by Eugene Kazaev in 2018-2020.
+// Distributed under the MIT license.
 //
 
 #if os(iOS) && canImport(SwiftUI)
