762e70eeaa7950f2f70cd2b831188b07517d53e6
Dov Goldberg <dov@viz.ai>
2022-02-13 11:26:20 +0200

    add keyWindow computed property to KeyWindowProvider.  Add ability to use tags to identify the main app window.

--- RouteComposer/Classes/Finders/Stack Iterator/KeyWindowProvider.swift
+++ RouteComposer/Classes/Finders/Stack Iterator/KeyWindowProvider.swift
@@ -16,9 +16,30 @@ import UIKit
 public struct KeyWindowProvider: WindowProvider {
 
     // MARK: Properties
+    
+    static let appWindowTag = 9999
 
-    /// `UIWindow` instance
+    /// `UIWindow` instance  /// returns the window with the appWindowTag
     public var window: UIWindow? {
+        let keyWindow: UIWindow?
+        if #available(iOS 13, *) {
+            let taggedWindow = UIApplication.shared.windows.first { $0.tag == KeyWindowProvider.appWindowTag }
+            if taggedWindow == nil {
+                keyWindow = UIApplication.shared.windows.first { $0.isKeyWindow }
+            } else {
+                keyWindow = taggedWindow
+            }
+        } else {
+            keyWindow = UIApplication.shared.keyWindow
+        }
+        guard let window = keyWindow else {
+            //assertionFailure("Application does not have a key window.")
+            return nil
+        }
+        return window
+    }
+    
+    public var keyWindow: UIWindow? {
         let keyWindow: UIWindow?
         if #available(iOS 13, *) {
             keyWindow = UIApplication.shared.windows.first { $0.isKeyWindow }
